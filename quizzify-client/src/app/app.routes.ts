09291895e92c6a24a900fc8e500adca58fbe50d5
Ibrahim-Abou-Al-Saud <ibrahimaboalsoud1@gmail.com>
2026-03-10 11:49:51 +0200

    update-routes

--- quizzify-client/src/app/app.routes.ts
+++ quizzify-client/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { authGuard } from './core/guards/auth.guard';
 import { Home } from './features/home/home';
 import { adminGuard } from './core/guards/admin.guard';
 import { userGuard } from './core/guards/user.guard';
+import { NotFound } from './shared/components/not-found/not-found';
+import { NotAuthorized } from './shared/components/not-authorized/not-authorized';
 
 export const routes: Routes = [
   {path: 'login', component: Login},
@@ -16,7 +18,8 @@ export const routes: Routes = [
       {path: 'home', component: Home},
       {path: 'admin', loadChildren: () => import('./features/modules/admin/admin-routing-module').then(m => m.AdminRoutingModule), canActivate: [adminGuard]},
       {path: 'user', loadChildren: () => import('./features/modules/user/user-routing-module').then(m => m.UserRoutingModule), canActivate: [userGuard]},
-      {path: 'not-found', loadComponent: () => import('./shared/components/not-found/not-found').then(m => m.NotFound)},
+      {path: 'not-found', component: NotFound},
+      {path: 'not-authorized', component: NotAuthorized},
       {path: '', redirectTo: 'home', pathMatch: 'full'},
       {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
   ]},
